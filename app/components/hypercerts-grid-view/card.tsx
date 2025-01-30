6a4292b69505ad3bf33c28f847cdd4fc5ff0d8c3
Satyam <2021pcecysatyam050@poornima.org>
2025-01-30 14:12:01 +0530

    fix: fix uneven padding in image and hypercert card boundaries on homepage

--- app/components/hypercerts-grid-view/card.tsx
+++ app/components/hypercerts-grid-view/card.tsx
@@ -26,17 +26,14 @@ const Card = ({ hypercert }: { hypercert: Hypercert }) => {
 	return (
 		<Link href={`/hypercert/${hypercertId}`} passHref>
 			<article className="group relative overflow-hidden rounded-2xl border border-border bg-muted">
-				<div className="h-[320px] w-full">
-					<div className="relative h-full w-full overflow-hidden">
-						<Image
-							// src={`/api/hypercert/${hypercert_id}/image`}
-							src={image ?? ""}
-							alt={name ?? "Untitled"}
-							fill
-							sizes="300px"
-							className="h-auto w-full object-contain object-center transition group-hover:scale-[1.05]"
-						/>
-					</div>
+				<div className="h-[320px] w-full overflow-hidden p-4">
+					<Image
+						src={image ?? ""}
+						alt={name ?? "Untitled"}
+						height={500}
+						width={500}
+						className="h-auto w-full object-contain object-center transition group-hover:scale-[1.05]"
+					/>
 				</div>
 				<section className="absolute top-4 left-4 flex space-x-1 opacity-100 transition-opacity duration-150 ease-out group-hover:opacity-100 md:opacity-0">
 					<div className="rounded-md border border-white/60 bg-black px-2 py-0.5 text-white text-xs shadow-sm">
