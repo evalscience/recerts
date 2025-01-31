a6cde62cd7d9b3dad01e80b78245266b9513ec17
Satyam <2021pcecysatyam050@poornima.org>
2025-01-31 15:44:46 +0530

    fix: use hyperboard ids in the config, rather then using env variables

--- config/hypercerts.ts
+++ config/hypercerts.ts
@@ -23,8 +23,11 @@ export const restEndpoint = HYPERCERTS_API_URL_REST;
 // Hypercerts Query Payload Config:
 
 //@TODO: Update the collection ids for prod and dev:
+const DEV_HYPERBOARD_ID = "3e42687a-ceeb-48f7-b8ab-99533ff0a81c";
+const PROD_HYPERBOARD_ID = "d8a64649-8ab7-4ac6-87d0-351de0df5eb0";
+
 export const collectionId = "0xa16DFb32Eb140a6f3F2AC68f41dAd8c7e83C4941";
 export const hyperboardId =
   process.env.NEXT_PUBLIC_DEPLOY_ENV === "production"
-    ? process.env.PROD_HYPERBOARD_ID
-    : process.env.DEV_HYPERBOARD_ID;
+    ? PROD_HYPERBOARD_ID
+    : DEV_HYPERBOARD_ID;
