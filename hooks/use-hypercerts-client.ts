07e138cebd471481f8cbb57830b2b43d4c285277
Satyam <2021pcecysatyam050@poornima.org>
2025-02-05 15:45:46 +0530

    fix: fix readonly client and redirection bugs

--- hooks/use-hypercerts-client.ts
+++ hooks/use-hypercerts-client.ts
@@ -4,17 +4,20 @@ import { useMemo } from "react";
 import { useWalletClient } from "wagmi";
 
 export const useHypercertClient = () => {
-	const { data: walletClient } = useWalletClient();
+  const { data: walletClient } = useWalletClient();
 
-	const client = useMemo(
-		() =>
-			new HypercertClient({
-				environment: `${process.env.NEXT_PUBLIC_DEPLOY_ENV} === 'production'` ? 'production' : 'test',
-				// @ts-ignore - wagmi and viem have different typing
-				walletClient,
-			}),
-		[walletClient],
-	);
+  const client = useMemo(
+    () =>
+      new HypercertClient({
+        environment:
+          process.env.NEXT_PUBLIC_DEPLOY_ENV === "production"
+            ? "production"
+            : "test",
+        // @ts-ignore - wagmi and viem have different typing
+        walletClient,
+      }),
+    [walletClient]
+  );
 
-	return { client };
+  return { client };
 };
