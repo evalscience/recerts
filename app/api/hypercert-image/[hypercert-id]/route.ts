67cb09ed5a7f13961150102fd9d3cb59a7b95439
Satyam <2021pcecysatyam050@poornima.org>
2025-02-27 21:28:51 +0530

    feat: add caching in the image fetching functions and api

--- app/api/hypercert-image/[hypercert-id]/route.ts
+++ app/api/hypercert-image/[hypercert-id]/route.ts
@@ -1,6 +1,11 @@
 import { type NextRequest, NextResponse } from "next/server";
 import { getHypercertImage } from "./get-hypercert-image";
 
+// The following line adds caching to this route handler
+export const dynamic = "force-static";
+// The following line revalidates the cache every 1800 seconds, i.e. 30 minutes
+export const revalidate = 1800;
+
 // GET handler to fetch and return the image associated with the given hypercert ID
 export async function GET(
 	request: NextRequest,
@@ -19,7 +24,7 @@ export async function GET(
 			status: 200,
 			headers: {
 				"Content-Type": contentType,
-				"Cache-Control": "s-maxage=864000", // 10 days cache
+				"Cache-Control": "s-maxage=1800", // 30 minutes cache
 			},
 		});
 	} catch (error) {
