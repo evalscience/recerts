4b993bbd747e2a0c3c5a552dfe37b9cb9257911d
Sharfy Adamantine <sharfy.adamantine@gmail.com>
2025-03-05 09:20:43 +0100

    Edit changelog

--- app/changelog/data.ts
+++ app/changelog/data.ts
@@ -2,21 +2,60 @@ import type { ChangeLogBlock } from "./config";
 
 const changelogBlocks: ChangeLogBlock[] = [
 	{
-		type: "github-release",
+		type: "normal",
 		title: "v1.0.0 - Initial Release",
 		description:
-			"Stable launch release for ecocertain. Users can create, buy, and view ecocerts on the Celo Network. Also supports Sepolia for testing environments.",
-		date: "6 Feb, 2025",
-		linkToRelease:
-			"https://github.com/GainForest/hypercerts-platform/releases/tag/v1.0.0",
+			"Code optimization: Hypercerts image fetching, caching and error handling.",
+		date: "February 27th 2025",
+		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/77",
 	},
 	{
 		type: "normal",
 		title: "v1.0.0 - Initial Release",
-		description: "We are soon preparing",
-		date: "6 Feb, 2025",
-		linkToPullRequest:
-			"https://github.com/GainForest/hypercerts-platform/pull/1",
+		description:
+			"UX improvements: Hypercert minting flow, improved error messages, and form handling.",
+		date: "February 26th 2025",
+		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/68",
+	},
+	{
+		type: "normal",
+		title: "v1.0.0 - Initial Release",
+		description:
+			"Major feature: Each hypercert's data explorer now queries the green globe, instead of impact trace.",
+		date: "February 26th 2025",
+		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/73",
+	},
+	{
+		type: "normal",
+		title: "v1.0.0 - Initial Release",
+		description:
+			'Feature improvement: Move the "My Hypercerts" page to the navbar, where it is more visible. Differentiate between created and supported hypercerts. ',
+		date: "February 13th 2025",
+		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/63",
+	},
+	{
+		type: "normal",
+		title: "v1.0.0 - Initial Release",
+		description: "Minor UI/UX improvement: Display sales chronologically. ",
+		date: "February 12th 2025",
+		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/62",
+	},
+	{
+		type: "normal",
+		title: "v1.0.0 - Initial Release",
+		description:
+			"Funds are now displayed in the original currency, instead of its USD equivalent. ",
+		date: "February 10th 2025",
+		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/62",
+	},
+	{
+		type: "github-release",
+		title: "v1.0.0 - Initial Release",
+		description:
+			"Stable launch release for ecocertain. Users can create, buy, and view ecocerts on the Celo Network. Also supports Sepolia for testing environments.",
+		date: "February 6th 2025",
+		linkToRelease:
+			"https://github.com/GainForest/hypercerts-platform/releases/tag/v1.0.0",
 	},
 ];
 
