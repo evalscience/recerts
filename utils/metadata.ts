01d773fbef3f5c22500a3f9525019baa9b4df06b
Satyam Mishra <satyam1308mishra@gmail.com>
2025-04-15 19:39:49 +0530

    feat: add view geojson button on site boundaries

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -1,13 +1,17 @@
 import { validateMetaData } from "@hypercerts-org/sdk";
 
+export const generateIPFSUrl = (cid: string) => {
+  return `https://ipfs.io/ipfs/${cid}`;
+};
+
 export const getMetadata = async (cid: string) => {
-	const res = await fetch(`https://ipfs.io/ipfs/${cid}`);
-	if (!res.ok) {
-		throw new Error(`IPFS fetch failed: ${res.statusText}`);
-	}
-	const data = await res.json();
+  const res = await fetch(generateIPFSUrl(cid));
+  if (!res.ok) {
+    throw new Error(`IPFS fetch failed: ${res.statusText}`);
+  }
+  const data = await res.json();
 
-	const validationResult = validateMetaData(data);
+  const validationResult = validateMetaData(data);
 
-	return validationResult;
+  return validationResult;
 };
