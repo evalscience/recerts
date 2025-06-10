a8d6257659976e023768dcdbdc9e75cdda429db6
Satyam Mishra <satyam1308mishra@gmail.com>
2025-06-10 22:04:35 +0530

    fix: add sdk and update farcaster.json

---
+++ app/components/FarcasterProvider.tsx
@@ -0,0 +1,12 @@
+import { sdk } from "@farcaster/frame-sdk";
+import type React from "react";
+import { useEffect } from "react";
+
+const FarcasterProvider = ({ children }: { children: React.ReactNode }) => {
+	useEffect(() => {
+		sdk.actions.ready();
+	}, []);
+	return <>{children}</>;
+};
+
+export default FarcasterProvider;
