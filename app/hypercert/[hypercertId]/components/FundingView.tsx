6303ae77c6011d33aeabd954ecdabead797c4349
Satyam Mishra <satyam1308mishra@gmail.com>
2025-07-03 20:34:31 +0530

    fix: use summation of sales to display the funding view on hypercert page

--- app/hypercert/[hypercertId]/components/FundingView.tsx
+++ app/hypercert/[hypercertId]/components/FundingView.tsx
@@ -5,36 +5,116 @@ import Progress from "@/app/components/shared/progress";
 import useFullHypercert from "@/app/contexts/full-hypercert";
 import type { FullHypercert } from "@/app/graphql-queries/hypercerts";
 import { Button } from "@/components/ui/button";
+import QuickTooltip from "@/components/ui/quicktooltip";
 import { calculateBigIntPercentage } from "@/lib/calculateBigIntPercentage";
-import { formatDecimals } from "@/lib/utils";
+import { convertCurrencyPriceToUSD, formatDecimals } from "@/lib/utils";
+import { cn } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
 import { ArrowRight, BadgeDollarSign, CircleAlert, Clock } from "lucide-react";
-import React from "react";
+import React, { useEffect } from "react";
 import { useAccount } from "wagmi";
 import PaymentFlow from "./PaymentFlow";
-const OpenVariant = ({ reached, goal }: { reached: number; goal: number }) => {
+
+const ProgressIndicator = ({
+	percentage,
+	tooltipText,
+	tooltipWidth,
+	direction,
+}: {
+	percentage: number;
+	tooltipText: string;
+	tooltipWidth?: number;
+	direction: "top" | "bottom";
+}) => {
 	return (
-		<div className="flex h-full w-full flex-col justify-between">
-			<div className="flex w-full flex-col">
-				<div className="relative z-[5] flex items-center justify-between">
-					<div className="flex flex-col items-start">
-						<span className="text-balance text-center text-muted-foreground">
-							Reached
-						</span>
-						<span className="font-bold text-xl">${reached}</span>
-					</div>
-					<div className="flex flex-col items-end">
-						<span className="text-balance text-center text-muted-foreground">
-							Goal
-						</span>
-						<span className="font-bold text-xl">${goal}</span>
+		<div
+			className={cn(
+				"relative mb-1 h-[12px] w-full",
+				direction === "top" ? "mb-1" : "mt-1",
+			)}
+		>
+			<div
+				className="-translate-x-1/2 absolute top-0 left-[-50%]"
+				style={{
+					left: `${percentage}%`,
+				}}
+			>
+				<div
+					className={cn(
+						"relative mx-auto h-0 w-0 scale-75 border-x-[8px] border-x-transparent transition-all duration-300 group-hover:scale-100",
+						direction === "top"
+							? "origin-bottom border-t-[12px] border-t-beige-muted-foreground"
+							: "origin-top border-b-[12px] border-b-primary/70",
+					)}
+				>
+					<div
+						className={cn(
+							"-translate-x-1/2 pointer-events-none absolute left-1/2 scale-0 rounded-full bg-background px-2 py-0.5 text-center font-bold text-sm opacity-0 shadow-md blur-xl transition-all duration-300 group-hover:scale-100 group-hover:opacity-100 group-hover:blur-none",
+							direction === "top"
+								? "bottom-[14px] mb-1 origin-bottom text-beige-muted-foreground"
+								: "top-[14px] mt-1 origin-top text-primary",
+						)}
+						style={{
+							width: tooltipWidth ? `${tooltipWidth}px` : undefined,
+						}}
+					>
+						{tooltipText}
 					</div>
 				</div>
-				<Progress
-					percentage={(reached * 100) / goal}
-					className="mt-2 bg-beige-muted"
-				/>
 			</div>
-			<div className="mt-4 flex items-center justify-end">
+		</div>
+	);
+};
+
+const OpenVariant = ({
+	totalSalesInUSD,
+	percentageUnitsSold,
+	goalInUSD,
+}: {
+	totalSalesInUSD: number;
+	percentageUnitsSold: number;
+	goalInUSD: number;
+}) => {
+	const goalPercentageOnBar =
+		totalSalesInUSD <= goalInUSD ? 100 : (goalInUSD / totalSalesInUSD) * 100;
+
+	return (
+		<div className="flex h-full w-full flex-col justify-between font-sans">
+			<div className="group flex w-full flex-col">
+				<div className="flex flex-col items-center">
+					<span className="font-bold text-3xl text-primary">
+						${totalSalesInUSD}
+					</span>
+					<span className="font-bold text-beige-muted-foreground text-xl">
+						out of ${goalInUSD}
+					</span>
+					<span className="font-bold text-muted-foreground text-sm">
+						sold so far.
+					</span>
+				</div>
+				<div className="mt-4 flex w-full flex-col items-center">
+					<div className="relative w-full">
+						<Progress
+							percentage={Math.min((totalSalesInUSD * 100) / goalInUSD, 100)}
+							className="h-6 rounded-sm bg-beige-muted"
+						/>
+						{goalPercentageOnBar < 99 && (
+							<div
+								className="-translate-x-[2px] absolute top-1 bottom-1 w-[4px] rounded-full bg-white shadow-md"
+								style={{
+									left: `${goalPercentageOnBar}%`,
+								}}
+							/>
+						)}
+					</div>
+					<ProgressIndicator
+						percentage={goalPercentageOnBar}
+						tooltipText={"Goal"}
+						direction="bottom"
+					/>
+				</div>
+			</div>
+			<div className="mt-2 flex items-center justify-end">
 				<PaymentFlow>
 					<Button className="gap-2" size={"sm"}>
 						Buy <ArrowRight size={16} />
@@ -105,8 +185,29 @@ const VariantSelector = () => {
 		totalUnits,
 		unitsForSale,
 		cheapestOrder: { pricePerPercentInUSD },
+		sales,
 	} = hypercert;
 	const { address } = useAccount();
+
+	const { data: totalSalesInUSD, isLoading } = useQuery({
+		queryKey: ["totalSalesInUSD", sales],
+		queryFn: async (): Promise<number> => {
+			if (!sales || sales.length === 0) return 0;
+			const salesInUSDPromises = sales.map(async (sale) => {
+				return convertCurrencyPriceToUSD(sale.currency, sale.currencyAmount);
+			});
+			const salesInUSD = await Promise.all(salesInUSDPromises);
+			let total = 0;
+			for (const sale of salesInUSD) {
+				if (sale !== null) {
+					total += sale;
+				}
+			}
+			return total;
+		},
+		enabled: pricePerPercentInUSD !== undefined && unitsForSale > 0n,
+	});
+
 	if (pricePerPercentInUSD === undefined) {
 		if (hypercert.creatorAddress.toLowerCase() === address?.toLowerCase()) {
 			return <ListingOptionVariant />;
@@ -117,10 +218,15 @@ const VariantSelector = () => {
 	const unitsSold = totalUnits - unitsForSale;
 	const percentCompleted = calculateBigIntPercentage(unitsSold, totalUnits);
 	if (percentCompleted === undefined) return <SoldVariant />;
+	if (isLoading || totalSalesInUSD === undefined) {
+		// Loading state, can show a spinner or just nothing
+		return null;
+	}
 	return (
 		<OpenVariant
-			reached={formatDecimals(percentCompleted * pricePerPercentInUSD)}
-			goal={formatDecimals(100 * pricePerPercentInUSD)}
+			percentageUnitsSold={percentCompleted}
+			goalInUSD={formatDecimals(100 * pricePerPercentInUSD)}
+			totalSalesInUSD={formatDecimals(totalSalesInUSD)}
 		/>
 	);
 };
--- lib/pricefeed.ts
+++ lib/pricefeed.ts
@@ -3,63 +3,67 @@ import { RAW_TOKENS_CONFIG } from "@/config/raw-tokens";
 const symbolToCurrencyAddressMap = new Map<string, `0x${string}`>();
 
 for (const chainId in RAW_TOKENS_CONFIG) {
-	const tokens = RAW_TOKENS_CONFIG[chainId];
-	for (const token of tokens) {
-		symbolToCurrencyAddressMap.set(
-			token.symbol,
-			token.address.toLowerCase() as `0x${string}`,
-		);
-	}
+  const tokens = RAW_TOKENS_CONFIG[chainId];
+  for (const token of tokens) {
+    symbolToCurrencyAddressMap.set(
+      token.symbol,
+      token.address.toLowerCase() as `0x${string}`
+    );
+  }
 }
 
 const currencyAddressToSymbolMap = new Map<`0x${string}`, string>();
 symbolToCurrencyAddressMap.forEach((value, key) => {
-	currencyAddressToSymbolMap.set(value, key);
+  currencyAddressToSymbolMap.set(value, key);
 });
 
 const currencyAddressToPriceFeedIdMap = new Map<string, number>([
-	[symbolToCurrencyAddressMap.get("CELO") ?? "", 5567],
+  [symbolToCurrencyAddressMap.get("CELO") ?? "", 5567],
 ]);
 
 type ApiResponse<Symbol extends string> = {
-	status: {
-		timestamp: string;
-		error_code: number;
-		error_message: unknown | "SUCCESS";
-		elapsed: string;
-		credit_count: number;
-	};
-	data?: {
-		symbol: Symbol;
-		id: string;
-		name: string;
-		amount: number;
-		last_updated: number;
-		quote: {
-			cryptoId: number;
-			symbol: string;
-			price: number;
-			lastUpdated: number;
-		}[];
-	};
+  status: {
+    timestamp: string;
+    error_code: number;
+    error_message: unknown | "SUCCESS";
+    elapsed: string;
+    credit_count: number;
+  };
+  data?: {
+    symbol: Symbol;
+    id: string;
+    name: string;
+    amount: number;
+    last_updated: number;
+    quote: {
+      cryptoId: number;
+      symbol: string;
+      price: number;
+      lastUpdated: number;
+    }[];
+  };
 };
 
 const getPriceFeed = async (currencyAddress: `0x${string}`) => {
-	const normalizedCurrencyAddress =
-		currencyAddress.toLowerCase() as `0x${string}`;
-	const priceFeedId = currencyAddressToPriceFeedIdMap.get(
-		normalizedCurrencyAddress,
-	);
-	const symbol = currencyAddressToSymbolMap.get(normalizedCurrencyAddress);
-	if (!priceFeedId || !symbol) {
-		return { usdPrice: null };
-	}
+  const normalizedCurrencyAddress =
+    currencyAddress.toLowerCase() as `0x${string}`;
+  const priceFeedId = currencyAddressToPriceFeedIdMap.get(
+    normalizedCurrencyAddress
+  );
+  const symbol = currencyAddressToSymbolMap.get(normalizedCurrencyAddress);
+  if (!priceFeedId || !symbol) {
+    return { usdPrice: null };
+  }
 
-	const response = await fetch(
-		`https://api.coinmarketcap.com/data-api/v3/tools/price-conversion?amount=1&convert_id=2781&id=${priceFeedId}`,
-	);
-	const data: ApiResponse<typeof symbol> = await response.json();
-	return { usdPrice: data?.data?.quote[0]?.price ?? null };
+  let priceFeedApiUrl = `https://api.coinmarketcap.com/data-api/v3/tools/price-conversion?amount=1&convert_id=2781&id=${priceFeedId}`;
+
+  if (typeof window !== "undefined") {
+    priceFeedApiUrl = `/api/price-conversion?id=${priceFeedId}&amount=1&convert_id=2781`;
+  }
+
+  const response = await fetch(priceFeedApiUrl);
+  const data: ApiResponse<typeof symbol> = await response.json();
+  return { usdPrice: data?.data?.quote[0]?.price ?? null };
 };
 
 export default getPriceFeed;
