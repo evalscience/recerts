116244c94633772556bbc03c132edac85cd8c350
daviddao <david@gainforest.net>
2025-08-26 21:00:15 +0200

    new farcaster style

--- app/changelog/data.ts
+++ app/changelog/data.ts
@@ -40,7 +40,7 @@ export default changelogBlocks;
 // 		description:
 // 			"Code optimization: Hypercerts image fetching, caching and error handling.",
 // 		date: "February 27th 2025",
-// 		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/77",
+// 		linkToPullRequest: "https://github.com/evalscience/recerts/pull/77",
 // 	},
 // 	{
 // 		type: "normal",
@@ -48,7 +48,7 @@ export default changelogBlocks;
 // 		description:
 // 			"UX improvements: Hypercert minting flow, improved error messages, and form handling.",
 // 		date: "February 26th 2025",
-// 		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/68",
+// 		linkToPullRequest: "https://github.com/evalscience/recerts/pull/68",
 // 	},
 // 	{
 // 		type: "normal",
@@ -56,7 +56,7 @@ export default changelogBlocks;
 // 		description:
 // 			"Major feature: Each hypercert's data explorer now queries the green globe, instead of impact trace.",
 // 		date: "February 26th 2025",
-// 		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/73",
+// 		linkToPullRequest: "https://github.com/evalscience/recerts/pull/73",
 // 	},
 // 	{
 // 		type: "normal",
@@ -64,14 +64,14 @@ export default changelogBlocks;
 // 		description:
 // 			'Feature improvement: Move the "My Hypercerts" page to the navbar, where it is more visible. Differentiate between created and supported hypercerts. ',
 // 		date: "February 13th 2025",
-// 		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/63",
+// 		linkToPullRequest: "https://github.com/evalscience/recerts/pull/63",
 // 	},
 // 	{
 // 		type: "normal",
 // 		title: "v1.0.0 - Initial Release",
 // 		description: "Minor UI/UX improvement: Display sales chronologically. ",
 // 		date: "February 12th 2025",
-// 		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/62",
+// 		linkToPullRequest: "https://github.com/evalscience/recerts/pull/62",
 // 	},
 // 	{
 // 		type: "normal",
@@ -79,13 +79,13 @@ export default changelogBlocks;
 // 		description:
 // 			"Funds are now displayed in the original currency, instead of its USD equivalent. ",
 // 		date: "February 10th 2025",
-// 		linkToPullRequest: "https://github.com/GainForest/ecocertain/pull/62",
+// 		linkToPullRequest: "https://github.com/evalscience/recerts/pull/62",
 // 	},
 // 	{
 // 		type: "github-release",
 // 		title: "v1.0.0 - Initial Release",
 // 		description:
-// 			"Stable launch release for ecocertain. Users can create, buy, and view ecocerts on the Celo Network. Also supports Sepolia for testing environments.",
+// 			"Stable launch release for recerts. Users can create, buy, and view recerts on the Celo Network. Also supports Sepolia for testing environments.",
 // 		date: "February 6th 2025",
 // 		linkToRelease:
 // 			"https://github.com/GainForest/hypercerts-platform/releases/tag/v1.0.0",
